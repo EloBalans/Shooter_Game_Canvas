7f836d1c0551042532b450ef63f92fa4093c82ed
Elo Balans <m.zaton@poczta.onet.pl>
2021-04-10 01:55:55 +0200

    alpha 5.3

--- js/skills.js
+++ js/skills.js
@@ -35,8 +35,13 @@ animate();
 
 function animate(){
     c.drawImage(skill10,skill10width,skillheight, skill10.width*1.2, skill10.height * 1.2);
+    c.save();
+    c.globalAlpha = 0.1;
     c.drawImage(skill1,skill1width,skillheight, skill1.width*1.2, skill1.height * 1.2);
+    c.restore();
     c.drawImage(skill2,skill2width,skillheight,skill2.width*1.2, skill2.height * 1.2);
+    c.save();
+    c.globalAlpha = 0.1
     c.drawImage(skill3,skill3width,skillheight,skill3.width*1.2, skill3.height * 1.2);
     c.drawImage(skill4,skill4width,skillheight,skill4.width*1.2, skill4.height * 1.2);
     c.drawImage(skill5,skill5width,skillheight,skill5.width*1.2, skill5.height * 1.2);
@@ -44,6 +49,7 @@ function animate(){
     c.drawImage(skill7,skill7width,skillheight,skill7.width*1.2, skill7.height * 1.2);
     c.drawImage(skill8,skill8width,skillheight,skill8.width*1.2, skill8.height * 1.2);
     c.drawImage(skill9,skill9width,skillheight,skill9.width*1.2, skill9.height * 1.2);
+    c.restore();
     
     c.drawImage(mouse,skill10width,skillheight+skill10.height*1.2+5,mouse.height/15,mouse.width/15)
     c.drawImage(keyboard, 337.5, 337.5, 112.5, 112.5, skill1width, skillheight+skill1.height*1.2+5, 38.4, 38.4);
@@ -56,5 +62,4 @@ function animate(){
     c.drawImage(keyboard, 562.5, 450, 112.5, 112.5, skill8width, skillheight+skill8.height*1.2+5, 38.4, 38.4);
     c.drawImage(keyboard, 675, 450, 112.5, 112.5, skill9width, skillheight+skill9.height*1.2+5, 38.4, 38.4);
 
-    requestAnimationFrame(animate);
 }
